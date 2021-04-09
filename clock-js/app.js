5439170d998a02712c49eea0645c65a08449a480
Nicolas Magno <nicolasmagno.100@gmail.com>
2021-04-09 20:13:02 -0300

    Final Project

--- clock-js/app.js
+++ clock-js/app.js
@@ -1,4 +1,5 @@
 //Selecionando os ponteiros no DOM
+const transitionPointer = document.querySelector('.ponteiro')
 const ponteiroSegundos = document.querySelector(`.ponteiro__sec`)
 const ponteiroMinutos = document.querySelector(`.ponteiro__min`)
 const ponteiroHoras = document.querySelector(`.ponteiro__hora`)
@@ -9,9 +10,22 @@ function setDate(){
 //Pegar os segundos atuais
     const sec = now.getSeconds()
 //Operação para dividir os 60 segundos dentro do circulo igualmente
-    const secGraus = ((sec / 60) * 360 )
-//O número "90", está somando para compensar o inicio dos ponteiros na posição horizontal
-    ponteiroSegundos.style.transform = `rotate(${secGraus + 90}deg)`
+    const secGraus = ((sec / 60) * 360)
+    //Este if se refere as ultimas e primeiras vezes que o ponteiro move
+    if(secGraus >= 355 || secGraus <= 10){
+        //Esta parte serve para evitar o bug de rotação do relógio causado pelo "transition"
+        ponteiroSegundos.style.transition = 'none'
+        ponteiroSegundos.style.transitionTimingFunction = 'unset'
+        ponteiroSegundos.style.transform = `rotate(${secGraus + 90}deg)`
+    } else if(secGraus < 350 || secGraus > 10){
+        //Efeito Tic-Tac do relógio
+        ponteiroSegundos.style.transition = '0.6s all'
+        ponteiroSegundos.style.transitionTimingFunction = 'cubic-bezier(0, 2.1, 0.25, 1)'
+        //O número "90", está somando para compensar o inicio dos ponteiros na posição horizontal
+        ponteiroSegundos.style.transform = `rotate(${secGraus + 90}deg)`
+    }
+        
+    
 
 //Pegar os minutos atuais
     const min = now.getMinutes()
