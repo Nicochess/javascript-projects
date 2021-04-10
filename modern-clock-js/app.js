d0db75b88394164c0662de027e8230c2878aa01b
Nicolas Magno <nicolasmagno.100@gmail.com>
2021-04-10 13:37:28 -0300

    JS Simple Function Working

--- modern-clock-js/app.js
+++ modern-clock-js/app.js
@@ -8,9 +8,9 @@ function setClock(){
     const minutes = dateNow.getMinutes()
     const seconds = dateNow.getSeconds()
 
-    clockSec.textContent = seconds
-    clockMin.textContent = minutes
-    clockHour.textContent = hour
+    seconds + 1 <= 10 ? clockSec.textContent = '0' + seconds : clockSec.textContent = seconds
+    minutes + 1 <= 10 ? clockMin.textContent = '0' + minutes : clockMin.textContent = minutes
+    hour + 1 <= 10 ? clockHour.textContent = '0' + hour : clockHour.textContent = hour
 }
 
 setInterval(setClock, 1000)
