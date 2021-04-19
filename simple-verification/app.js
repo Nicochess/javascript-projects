30f1736f04f887bc45de04bf0edb2dc2ca78fb4c
Nicolas Magno <nicolasmagno.100@gmail.com>
2021-04-19 20:57:56 -0300

    Complete Validation

--- simple-verification/app.js
+++ simple-verification/app.js
@@ -5,11 +5,24 @@ buttonLogin.addEventListener('click', event =>{
     
     //Form retorna um objeto com os campos dentro
     //Cada campo pode ser acessado a partir do 'name'
-    const formAdd = document.querySelector('#login')
-    const formObj = getDados(formAdd)
+    const form = document.querySelector('#login')
+    const formObj = getDados(form)
     console.log(formObj)
 
+    const erro = getErrors(formObj)
 
+    //Se possuir algum erro no preenchimento, ele executa a função de exibir estes erros e corta
+    //o fluxo do código com o 'return'
+    if(erro.length > 0){
+        showErrors(erro)
+        return
+    }
+
+    const ul = document.querySelector('#erros')
+
+    //Limpa a lista de erros antes de dar "reset" na página e começar um novo preenchimento
+    ul.innerHTML = ""
+    form.reset()
 })
 
 //Função criada para gerar um objeto com os dados do formulário
@@ -21,4 +34,42 @@ function getDados(form){
     }
 
     return dados
+}
+
+//Essa função irá verificar varias condições e armazenar cada erro em um array usando o metodo 'push'
+function getErrors(info){
+    const errors = []
+
+    if(info.user.length == 0){
+        errors.push('Campo usuário está vazio.')
+    }
+    
+    if(info.senha.length == 0){
+        errors.push('Campo senha está vazio.')
+    }
+
+    if(info.confirm.length == 0){
+        errors.push('Campo de confirmação de senha está vazio.')
+    }
+    
+    if(info.senha.length !== info.confirm.length){
+        errors.push('A senhas são diferentes, favor confirmar corretamente.')
+    }
+
+    return errors
+}
+
+
+//Essa função pega o array de mensagens com erros e adiciona elas em uma lista que será exibida na página
+function showErrors(erro){
+    const ul = document.querySelector('#erros')
+    //Limpa a lista para buscar novos erros
+    ul.innerHTML = ""
+    
+
+    erro.forEach(erro => {
+        const li = document.createElement('li')
+        li.textContent = erro
+        ul.appendChild(li)
+    })
 }
