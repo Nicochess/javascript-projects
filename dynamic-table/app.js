9dbde845dd4ad4d8a3b21942346b1e6094b26680
Nicolas Magno <nicolasmagno.100@gmail.com>
2021-04-22 16:21:48 -0300

    Create app.js

---
+++ dynamic-table/app.js
@@ -0,0 +1,81 @@
+const buttonSave = document.querySelector('#save-datas')
+
+buttonSave.addEventListener('click',event=>{
+    event.preventDefault()
+
+    const form = document.querySelector('#form-datas')
+    const formObj = addObj(form)
+    
+    const erros = validationForm(formObj)
+
+    if(erros.length > 0){
+        showErro(erros)
+        return
+    }
+
+    putDatas(formObj)
+    const ul = document.querySelector('.erros')
+    ul.innerHTML = ''
+
+    form.reset()
+})
+
+
+function addObj(form){
+    const obj = {
+        site: form.site.value,
+        email: form.email.value,
+        senha: form.senha.value
+    }
+
+    return obj
+}
+
+function putDatas(obj){
+    const table = document.querySelector('#data-table')
+
+    const tr = document.createElement('tr')
+
+    tr.appendChild(createTD(obj.site, 'info-site'))
+    tr.appendChild(createTD(obj.email, 'info-email'))
+    tr.appendChild(createTD(obj.senha, 'info-senha'))
+
+    return table.appendChild(tr)
+}
+
+function createTD(data, classe){
+    const td = document.createElement('td')
+    td.textContent = data
+    td.classList.add(classe)
+
+    return td
+}
+
+function validationForm(datas){
+    const erro = []
+
+    if(datas.site == ''){
+        erro.push('Você não inseriu o site desejado.')
+    }
+
+    if(datas.email == ''){
+        erro.push('Esqueceu de salvar seu email?')
+    }
+
+    if(datas.senha == ''){
+        erro.push('Não esqueça de salvar sua senha!')
+    }
+
+    return erro
+}  
+
+function showErro(erro){
+    const ul = document.querySelector('.erros')
+    ul.innerHTML = ''
+    
+    erro.forEach(erro => {
+        const li = document.createElement('li')
+        li.textContent = erro
+        ul.appendChild(li)
+    });
+}
