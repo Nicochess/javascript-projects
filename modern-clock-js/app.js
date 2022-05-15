7765b9ef741eaaa4236af92bd209376762ab52e7
Nicolas Magno <66505477+Nicochess@users.noreply.github.com>
2022-05-15 13:57:58 -0300

    feat: object literals structure

--- modern-clock-js/app.js
+++ modern-clock-js/app.js
@@ -1,6 +1,8 @@
-const clockHour = document.querySelector('.hours')
-const clockMin = document.querySelector('.minutes')
-const clockSec = document.querySelector('.seconds')
+const clock = {
+    hour: document.querySelector('.hours'),
+    min: document.querySelector('.minutes'),
+    sec: document.querySelector('.seconds')
+}
 
 function setClock(){
     const dateNow = new Date()
@@ -8,9 +10,9 @@ function setClock(){
     const minutes = dateNow.getMinutes()
     const seconds = dateNow.getSeconds()
 
-    seconds + 1 <= 10 ? clockSec.textContent = '0' + seconds : clockSec.textContent = seconds
-    minutes + 1 <= 10 ? clockMin.textContent = '0' + minutes : clockMin.textContent = minutes
-    hour + 1 <= 10 ? clockHour.textContent = '0' + hour : clockHour.textContent = hour
+    seconds + 1 <= 10 ? clock.sec.textContent = '0' + seconds : clock.sec.textContent = seconds
+    minutes + 1 <= 10 ? clock.min.textContent = '0' + minutes : clock.min.textContent = minutes
+    hour + 1 <= 10 ? clock.hour.textContent = '0' + hour : clock.hour.textContent = hour
 }
 
 setInterval(setClock, 1000)
